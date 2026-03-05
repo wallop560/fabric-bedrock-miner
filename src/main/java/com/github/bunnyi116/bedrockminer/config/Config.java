51fbd5bbafae8a94cbd3c90aae66bf102880c0c1
wallop560 <lukesupreem@gmail.com>
2026-03-05 21:39:39 +0000

    jst a quick dirty lil fork. removed the guardrails

--- src/main/java/com/github/bunnyi116/bedrockminer/config/Config.java
+++ src/main/java/com/github/bunnyi116/bedrockminer/config/Config.java
@@ -32,22 +32,25 @@ public class Config {
         var list = new ArrayList<String>();
         list.add(BlockUtils.getIdentifierString(Blocks.BEDROCK));                  // 基岩
         // 防误触，取消以下默认白名单
-        // list.addTask(BlockUtils.getId(Blocks.END_PORTAL));               // 末地传送门
-        // list.addTask(BlockUtils.getId(Blocks.END_PORTAL_FRAME));         // 末地传送门-框架
-        // list.addTask(BlockUtils.getId(Blocks.END_GATEWAY));              // 末地折跃门
+        // >:)
+        list.addTask(BlockUtils.getId(Blocks.END_PORTAL));               // 末地传送门
+        list.addTask(BlockUtils.getId(Blocks.END_PORTAL_FRAME));         // 末地传送门-框架
+        list.addTask(BlockUtils.getId(Blocks.END_GATEWAY));              // 末地折跃门
         return list;
     }
 
     public static List<String> getDefaultBlockBlacklistServer() {
         // 默认方块黑名单 (用于限制的服务器, 与自定义黑名单分离)
         var list = new ArrayList<String>();
-        list.add(BlockUtils.getIdentifierString(Blocks.BARRIER));                    // 屏障
-        list.add(BlockUtils.getIdentifierString(Blocks.COMMAND_BLOCK));              // 普通命令方块
-        list.add(BlockUtils.getIdentifierString(Blocks.CHAIN_COMMAND_BLOCK));        // 连锁型命令方块
-        list.add(BlockUtils.getIdentifierString(Blocks.REPEATING_COMMAND_BLOCK));    // 循环型命令方块
-        list.add(BlockUtils.getIdentifierString(Blocks.STRUCTURE_VOID));             // 结构空位
-        list.add(BlockUtils.getIdentifierString(Blocks.STRUCTURE_BLOCK));            // 结构方块
-        list.add(BlockUtils.getIdentifierString(Blocks.JIGSAW));                     // 拼图方块
+        // >:)
+
+        //list.add(BlockUtils.getIdentifierString(Blocks.BARRIER));                    // 屏障
+        //list.add(BlockUtils.getIdentifierString(Blocks.COMMAND_BLOCK));              // 普通命令方块
+        //list.add(BlockUtils.getIdentifierString(Blocks.CHAIN_COMMAND_BLOCK));        // 连锁型命令方块
+        //list.add(BlockUtils.getIdentifierString(Blocks.REPEATING_COMMAND_BLOCK));    // 循环型命令方块
+        //list.add(BlockUtils.getIdentifierString(Blocks.STRUCTURE_VOID));             // 结构空位
+        //list.add(BlockUtils.getIdentifierString(Blocks.STRUCTURE_BLOCK));            // 结构方块
+        //list.add(BlockUtils.getIdentifierString(Blocks.JIGSAW));                     // 拼图方块
         return list;
     }
 
